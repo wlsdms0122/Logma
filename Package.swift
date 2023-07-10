2aee054c925ba052bb725409a1f5ba9d25c91a5c
Jeong Jineun <wlsdms0122@gmail.com>
2023-07-10 14:19:03 +0900

    feat(): rename to `Logma`

--- Package.swift
+++ Package.swift
@@ -1,23 +1,23 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Logger",
+    name: "Logma",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v14)
     ],
     products: [
         .library(
-            name: "Logger",
-            targets: ["Logger"]
+            name: "Logma",
+            targets: ["Logma"]
         )
     ],
     dependencies: [],
     targets: [
         .target(
-            name: "Logger",
+            name: "Logma",
             dependencies: []
         )
     ]
