1da7c00272f1f2d80612a2b945b53d040f947e46
Jineun Jeong <wlsdms0122@gmail.com>
2024-01-22 17:20:37 +0900

    fix(): fix try await order syntax warning

--- Sources/Logmo/Logmo.swift
+++ Sources/Logmo/Logmo.swift
@@ -75,7 +75,7 @@ public class Logmo: ObservableObject {
         isExporting = true
         
         Task { [weak self] in
-            guard let file = await try? LogFile(logs) else {
+            guard let file = try? await LogFile(logs) else {
                 self?.isExporting = false
                 return
             }
