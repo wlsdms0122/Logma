fe368249a630bf28849bb405ce162feffdb775a5
Jineun Jeong <wlsdms0122@gmail.com>
2024-11-24 20:24:04 +0900

    fix(): fix os version to read from process info

--- Sources/Logmo/Model/Environment.swift
+++ Sources/Logmo/Model/Environment.swift
@@ -1,6 +1,6 @@
 //
 //  Environment.swift
-//  
+//
 //
 //  Created by JSilver on 2023/07/17.
 //
@@ -9,28 +9,35 @@ import Foundation
 
 typealias Env = Environment
 
-enum Environment { }
+enum Environment {}
 
 extension Environment {
     static var infoPlist: [String: Any]? { Bundle.main.infoDictionary }
-    
+
     static var version: Version? {
-        guard let versionString = infoPlist?["CFBundleShortVersionString"] as? String else { return nil }
+        guard let versionString = infoPlist?["CFBundleShortVersionString"] as? String else {
+            return nil
+        }
         return Version(versionString)
     }
-    
+
     static var build: String? {
         infoPlist?["CFBundleVersion"] as? String
     }
-    
-    static var osVersion: String? {
-        infoPlist?["DTPlatformVersion"] as? String
+
+    static var osVersion: Version {
+        let version = ProcessInfo.processInfo.operatingSystemVersion
+        return Version(
+            major: version.majorVersion,
+            minor: version.minorVersion,
+            patch: version.patchVersion
+        )
     }
-    
+
     static var platformName: String? {
         infoPlist?["DTPlatformName"] as? String
     }
-    
+
     static var deviceName: String {
         var systemInfo = utsname()
         uname(&systemInfo)
@@ -39,22 +46,22 @@ extension Environment {
             guard let value = element.value as? Int8, value != 0 else { return identifier }
             return identifier + String(UnicodeScalar(UInt8(value)))
         }
-        
+
         return identifier
     }
-    
+
     static var productName: String? {
         infoPlist?["CFBundleName"] as? String
     }
-    
+
     static var bundleIdentifier: String? {
         infoPlist?["CFBundleIdentifier"] as? String
     }
-    
+
     static var bundleDisplayName: String? {
         infoPlist?["CFBundleDisplayName"] as? String
     }
-    
+
     static var region: String? {
         infoPlist?["CFBundleDevelopmentRegion"] as? String
     }
@@ -62,5 +69,7 @@ extension Environment {
 
 extension Environment {
     static var license: String { "MIT" }
-    static var licenseURL: URL? { URL(string: "https://github.com/wlsdms0122/Logma/blob/main/LICENSE") }
+    static var licenseURL: URL? {
+        URL(string: "https://github.com/wlsdms0122/Logma/blob/main/LICENSE")
+    }
 }
